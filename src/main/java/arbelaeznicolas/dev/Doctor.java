59bdff7576bdc6ae9297c6741c74cf2762fc1f3d
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-12 14:58:59 -0500

    Add class changes #6
    https://platzi.com/home/clases/1629-java-oop/21581-metodo-constructor/

--- src/main/java/arbelaeznicolas/dev/Doctor.java
+++ src/main/java/arbelaeznicolas/dev/Doctor.java
@@ -5,6 +5,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor() {
+        System.out.println("Construyendo el objeto Doctor");
+    }
+
+    Doctor(String name) {
+        System.out.println("El nombre del doctor asignado es: " + name);
+    }
+
     public void showName() {
         System.out.println(name);
     }
