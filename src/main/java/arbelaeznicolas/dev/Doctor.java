3d638d2fb57684d3af9226908f17610b0dd2acbd
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-12 15:17:21 -0500

    Add class changes #7 (no merge)
    https://platzi.com/home/clases/1629-java-oop/21582-static-variables-y-metodos-estaticos/

--- src/main/java/arbelaeznicolas/dev/Doctor.java
+++ src/main/java/arbelaeznicolas/dev/Doctor.java
@@ -1,19 +1,26 @@
 package arbelaeznicolas.dev;
 
 public class Doctor {
+    static int globalId = 0;
     int id;
     String name;
     String speciality;
 
     Doctor() {
+        this.id = ++globalId;
         System.out.println("Construyendo el objeto Doctor");
     }
-
+    
     Doctor(String name) {
+        this.id = ++globalId;
         System.out.println("El nombre del doctor asignado es: " + name);
     }
 
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("El id del doctor es: " + id);
     }
 }
--- src/main/java/arbelaeznicolas/dev/Main.java
+++ src/main/java/arbelaeznicolas/dev/Main.java
@@ -5,5 +5,11 @@ public class Main {
         Doctor myDoctor = new Doctor("Anahí Salgado");
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctor2 = new Doctor();
+        myDoctor2.name = "Anahí Salgado";
+        myDoctor2.showName();
+        myDoctor2.showId();
     }
 }
