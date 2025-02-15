833cdd7cecf2edc398918119bff857fbb8712541
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-14 22:22:48 -0500

    Add class changes #9
    https://platzi.com/home/clases/1629-java-oop/21584-final-variables-constantes/

--- src/main/java/arbelaeznicolas/dev/Main.java
+++ src/main/java/arbelaeznicolas/dev/Main.java
@@ -4,14 +4,6 @@ import static arbelaeznicolas.dev.ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodríguez";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctorAnn.showId();
-
         showMenu();
     }
 }
