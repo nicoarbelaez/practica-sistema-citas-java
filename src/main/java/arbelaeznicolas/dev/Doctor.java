7c9e66c066b483850122ac6bdd7efb7b7550447c
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-14 22:40:57 -0500

    Add class changes #10
    https://platzi.com/home/clases/1629-java-oop/21585-sobrecarga-de-metodos-y-constructores/

--- src/main/java/arbelaeznicolas/dev/Doctor.java
+++ src/main/java/arbelaeznicolas/dev/Doctor.java
@@ -7,11 +7,13 @@ public class Doctor {
 
     Doctor() {
         System.out.println("Construyendo el objeto Doctor");
-        id++;
     }
 
-    Doctor(String name) {
+    Doctor(String name, String speciality) {
         System.out.println("El nombre del doctor asignado es: " + name);
+        id++;
+        this.name = name;
+        this.speciality = speciality;
     }
 
     public void showName() {
--- src/main/java/arbelaeznicolas/dev/Main.java
+++ src/main/java/arbelaeznicolas/dev/Main.java
@@ -4,6 +4,10 @@ import static arbelaeznicolas.dev.ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        showMenu();
+        // showMenu();
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatría");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 }
