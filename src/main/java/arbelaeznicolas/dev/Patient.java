70fed30d64150b35653cd9e7aec92a7a6bb21e3d
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-14 22:47:40 -0500

    Add class changes #11
    https://platzi.com/home/clases/1629-java-oop/21589-reto/

---
+++ src/main/java/arbelaeznicolas/dev/Patient.java
@@ -0,0 +1,18 @@
+package arbelaeznicolas.dev;
+
+public class Patient {
+    public String name;
+    public String email;
+    public String address;
+    public String phoneNumber;
+    public String birthday;
+    public double weight;
+    public double height;
+    public String blood;
+
+    Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+}
