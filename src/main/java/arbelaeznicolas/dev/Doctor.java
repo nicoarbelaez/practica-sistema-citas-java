a5dc2226a8601f16febb866c8e6ac43b475045aa
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-15 12:07:01 -0500

    Add class changes #16
    https://platzi.com/home/clases/1629-java-oop/21590-clases-anidadas/

--- src/main/java/arbelaeznicolas/dev/Doctor.java
+++ src/main/java/arbelaeznicolas/dev/Doctor.java
@@ -1,9 +1,12 @@
 package arbelaeznicolas.dev;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id;
-    String name;
-    String speciality;
+    private String name;
+    private String speciality;
 
     Doctor() {
         System.out.println("Construyendo el objeto Doctor");
@@ -22,5 +25,50 @@ public class Doctor {
 
     public void showId() {
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment {
+        private int idAvailableAppointments;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getIdAvailableAppointments() {
+            return idAvailableAppointments;
+        }
+
+        public void setIdAvailableAppointments(int idAvailableAppointments) {
+            this.idAvailableAppointments = idAvailableAppointments;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/main/java/arbelaeznicolas/dev/Main.java
+++ src/main/java/arbelaeznicolas/dev/Main.java
@@ -2,44 +2,34 @@ package arbelaeznicolas.dev;
 
 import static arbelaeznicolas.dev.ui.UIMenu.*;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         // showMenu();
 
         Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatría");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        int i = 0;
-        int b = 2;
-        b = i;
-        System.out.println(b);
-        System.out.println(i);
-        i = 3;
-        System.out.println(b);
-        System.out.println(i);
-        
-        String name = "Ann";
+        for (Doctor.AvailableAppointment aAppointment : myDoctor.getAvailableAppointments()) {
+            System.out.println(aAppointment.getDate() + " " + aAppointment.getTime());
+        }
 
-        System.out.println("\n");
-        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
-        Patient patient2 = new Patient("Anahi", "anahi@mail.com");
+        // System.out.println("\n");
+        // Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        // Patient patient2 = new Patient("Anahi", "anahi@mail.com");
 
-        System.out.println(patient.getName());
-        System.out.println(patient2.getName());
-        patient2 = patient;
+        // System.out.println(patient.getName());
+        // System.out.println(patient2.getName());
+        // patient2 = patient;
 
-        System.out.println(patient.getName());
-        System.out.println(patient2.getName());
+        // System.out.println(patient.getName());
+        // System.out.println(patient2.getName());
 
-        patient.setName("Manuel");
-        System.out.println(patient.getName());
-        System.out.println(patient2.getName());
-
-        // patient.setWeight(54.6);
-        // System.out.println(patient.getWeight());
-
-        // patient.setPhoneNumber("12345678");
-        // System.out.println(patient.getPhoneNumber());
+        // patient.setName("Manuel");
+        // System.out.println(patient.getName());
+        // System.out.println(patient2.getName());
     }
 }
