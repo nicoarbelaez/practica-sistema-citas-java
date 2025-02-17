1a749fc85ac520ab4d2929e77ede18dfb0c8319b
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-16 20:29:26 -0500

    Add class changes #33
    https://platzi.com/home/clases/1629-java-oop/21605-modularizando-la-ui-de-doctores/

---
+++ src/main/java/arbelaeznicolas/dev/ui/UIDoctorMenu.java
@@ -0,0 +1,62 @@
+package arbelaeznicolas.dev.ui;
+
+import java.util.Scanner;
+
+public class UIDoctorMenu {
+    public static void showDoctorMenu() {
+        int response = 0;
+        do {
+            System.out.println("\n\n");
+            System.out.println("Doctor");
+            System.out.println("Welcome " + UIMenu.doctorLogged.getName());
+            System.out.println("1. Add Available Appointment");
+            System.out.println("2. My Scheduled Appointments");
+            System.out.println("0. Logout");
+
+            Scanner sc = new Scanner(System.in);
+            response = sc.nextInt();
+
+            switch (response) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    UIMenu.showMenu();
+                    break;
+            }
+            sc.close();
+        } while (response != 0);
+    }
+
+    private static void showAddAvailableAppointmentsMenu() {
+        int response = 0;
+        do {
+            System.out.println();
+            System.out.println("::Add Available Appointment");
+            System.out.println("::Select a Month");
+
+            for (int i = 0; i < 3; i++) {
+                int j = i + 1;
+                System.out.println(j + ". " + UIMenu.MONTHS[i]);
+            }
+            System.out.println("0. Return");
+
+            Scanner sc = new Scanner(System.in);
+            response = sc.nextInt();
+
+            if (response > 0 && response < 4) {
+                int monthSelected = response;
+                System.out.println(monthSelected + ". " + UIMenu.MONTHS[monthSelected - 1]);
+
+                System.out.println("Insert the date available: [dd/mm/yyyy]");
+                String date = sc.next();
+
+                System.out.println("Your date is: " + date + "\n1. Correct \n2. Change Date");
+            } else if (response == 0) {
+                showDoctorMenu();
+            }
+            sc.close();
+        } while (response != 0);
+    }
+}
