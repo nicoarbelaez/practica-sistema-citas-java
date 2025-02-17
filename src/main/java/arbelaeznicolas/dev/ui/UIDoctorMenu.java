0adf46e2dd397a299ff94d229586a3cd1bda62a1
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-16 20:42:46 -0500

    Add class changes #34
    https://platzi.com/home/clases/1629-java-oop/21606-definiendo-las-citas-disponibles/

--- src/main/java/arbelaeznicolas/dev/ui/UIDoctorMenu.java
+++ src/main/java/arbelaeznicolas/dev/ui/UIDoctorMenu.java
@@ -1,8 +1,13 @@
 package arbelaeznicolas.dev.ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import arbelaeznicolas.dev.model.Doctor;
+
 public class UIDoctorMenu {
+    public static ArrayList<Doctor> doctorsAvailableAppointments = new ArrayList<>();
+
     public static void showDoctorMenu() {
         int response = 0;
         do {
@@ -53,10 +58,31 @@ public class UIDoctorMenu {
                 String date = sc.next();
 
                 System.out.println("Your date is: " + date + "\n1. Correct \n2. Change Date");
+                int responseDate = sc.nextInt();
+
+                if (responseDate == 2)
+                    continue;
+
+                int responseTime = 0;
+                String time = "";
+                do {
+                    System.out.println("Insert the time available for date: " + date + " [hh:mm]");
+                    time = sc.next();
+                    System.out.println("Your time is: " + time + "\n1. Correct \n2. Change Time");
+                    responseTime = sc.nextInt();
+                } while (responseTime == 2);
+
+                UIMenu.doctorLogged.addAvailableAppointment(date, time);
             } else if (response == 0) {
                 showDoctorMenu();
             }
             sc.close();
         } while (response != 0);
+    }
+
+    private static void checkDoctorAvailableAppointments(Doctor doctor) {
+        if (doctor.getAvailableAppointments().size() > 0 && !doctorsAvailableAppointments.contains(doctor)) {
+            doctorsAvailableAppointments.add(doctor);
+        }
     }
 }
